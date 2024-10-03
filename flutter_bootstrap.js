a264dd3018acd9c4c0c93f890f1cc7ed4a4861f9
Thai Parkerizing <administrator@syn-172-101-034-209.res.spectrum.com>
2024-10-03 13:33:11 +0700

    .

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4149197891"
+    serviceWorkerVersion: "784221624"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "23969eb55c7530e25fa03a093f7481ec",
+const RESOURCES = {"flutter_bootstrap.js": "7cd3bfa514183f82699400f241d58d99",
 "version.json": "7f86edf6c13624ed15391b7ecd82e7d0",
 "index.html": "5216f2958ee3c3848a75867a246f20ba",
 "/": "5216f2958ee3c3848a75867a246f20ba",
-"main.dart.js": "52b2b09e4cbc06d416dbd9043eb3509d",
+"main.dart.js": "7147ae15c6aacdead8c8975eeeba1075",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
